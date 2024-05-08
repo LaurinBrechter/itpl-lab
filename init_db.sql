1fcb7a92c8ead3cee13d857941064a65a620c466
g0glus <jan.khalo@gmx.de>
2024-05-08 19:45:59 +0200

    HeidiSQL DB export eingefügt

--- init_db.sql
+++ init_db.sql
@@ -1,33 +1,112 @@
-CREATE TABLE products (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    price DECIMAL(10, 2),
-    production_duration INT,
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    storage_amount INT,
-);
+CREATE TABLE IF NOT EXISTS `addresses` (
+  `address_id` int(11) NOT NULL AUTO_INCREMENT,
+  `street` varchar(50) DEFAULT NULL,
+  `house_number` varchar(10) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `zip` varchar(10) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`address_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE storage_logs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    product_id INT,
-    storage_id INT,
-    order_id INT,
-    amount INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS `customers` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) DEFAULT NULL,
+  `address_id` int(11) DEFAULT NULL,
+  `telephone_number` varchar(20) DEFAULT NULL,
+  `isVip` enum('Yes','No') DEFAULT NULL,
+  PRIMARY KEY (`customer_id`),
+  KEY `address_id2` (`address_id`),
+  CONSTRAINT `address_id2` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`address_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE table orders (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    sp_id INT, -- service partner id
-    status ENUM('PENDING', 'COMPLETED', 'CANCELLED'),
-);
+CREATE TABLE IF NOT EXISTS `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `priority` enum('High','Medium','low') NOT NULL,
+  `sp_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `status` enum('PENDING','COMPLETED','CANCELLED') DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`order_id`) USING BTREE,
+  KEY `customer_id` (`customer_id`),
+  KEY `sp_id` (`sp_id`),
+  CONSTRAINT `customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sp_id` FOREIGN KEY (`sp_id`) REFERENCES `servicepartners` (`sp_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE order_items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    product_id INT,
-    amount INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS `order_items` (
+  `item_count_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) DEFAULT NULL,
+  `sku` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`item_count_id`) USING BTREE,
+  KEY `order_id` (`order_id`),
+  KEY `sku2` (`sku`),
+  CONSTRAINT `order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sku2` FOREIGN KEY (`sku`) REFERENCES `products` (`sku`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `productionplan` (
+  `sku` int(11) DEFAULT NULL,
+  `production_amt` int(11) DEFAULT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `status` enum('PENDING','COMPLETED','CANCELLED') DEFAULT NULL,
+  `next_production_item_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  KEY `sku` (`sku`),
+  KEY `order_id2` (`order_id`),
+  CONSTRAINT `order_id2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sku` FOREIGN KEY (`sku`) REFERENCES `products` (`sku`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `production_facilities` (
+  `production_facility_id` int(11) NOT NULL AUTO_INCREMENT,
+  `address_id` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`production_facility_id`),
+  KEY `address_id 4` (`address_id`),
+  CONSTRAINT `address_id 4` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`address_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `sku` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `production_duration` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `storage_amount` int(11) DEFAULT NULL,
+  PRIMARY KEY (`sku`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `servicepartners` (
+  `sp_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `tax_number` varchar(20) DEFAULT NULL,
+  `address_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`sp_id`),
+  KEY `address_id` (`address_id`),
+  CONSTRAINT `address_id` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`address_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `storage_facilities` (
+  `storage_facility_id` int(11) NOT NULL AUTO_INCREMENT,
+  `address_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`storage_facility_id`),
+  KEY `address_id3` (`address_id`),
+  CONSTRAINT `address_id3` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`address_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `storage_log` (
+  `storage_event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sku` int(11) DEFAULT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `amount` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`storage_event_id`) USING BTREE,
+  KEY `sku3` (`sku`),
+  KEY `order_id3` (`order_id`),
+  CONSTRAINT `order_id3` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sku3` FOREIGN KEY (`sku`) REFERENCES `products` (`sku`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
