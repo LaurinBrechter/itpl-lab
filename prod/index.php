1fd71fc71519dce3e24905e97bb723891d80437a
laurinbrechter <brechterlaurin@gmail.com>
2024-05-12 14:10:10 +0200

    update production page

--- prod/index.php
+++ prod/index.php
@@ -8,60 +8,106 @@
     <style>
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/style.css'; ?>
     </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 
 <body>
-    <?php include $_SERVER['DOCUMENT_ROOT'] . '/prod/prod_navbar.php'; ?>
-    <form>
-        <button type="submit">Save</button>
+    <?php
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/prod/prod_navbar.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/database.php';
+
+    $production_plan = $conn->query("SELECT * FROM production_plan order by status asc, priority desc;");
+
+    ?>
+
+    <form id="production_form" method="GET" onsubmit="event.preventDefault()">
+        <h2>Next Item</h2>
+
+        <table>
+
+            <?php
+
+            if ($production_plan->num_rows > 0) {
+
+                $row = $production_plan->fetch_assoc();
+                if ($row['status'] == 'PENDING') {
+                    echo
+                        "<thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Quantity</th>
+                            <th>Created At</th>
+                            <th>Production Status</th>
+                            <th>Priority</th>
+                            <th>Target</th>
+                            <th>Product Id</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>";
+                    echo "<tr>";
+                    echo "<input type='hidden' name='id' value='" . $row['id'] . "'>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['amount'] . "</td>";
+                    echo "<td>" . $row['created_at'] . "</td>";
+                    echo "<td>" . $row['status'] . "</td>";
+                    echo "<td>" . $row['priority'] . "</td>";
+                    echo "<td>" . $row['target'] . "</td>";
+                    echo "<td>" . $row["product_id"] . "</td>";
+                    echo "<td><button type='submit'>Complete</button></td>";
+                    echo "</tr>";
+                } else {
+                    echo "<tr>";
+                    echo "<td>No open items</td>";
+                    echo "</tr>";
+                }
+            }
+            ?>
+        </table>
     </form>
-    <table>
+    <h2>Production History</h2>
+    <table class="production-item-table">
         <thead>
             <tr>
-                <th>SKU</th>
+                <th>Id</th>
                 <th>Quantity</th>
                 <th>Created At</th>
                 <th>Production Status</th>
+                <th>Priority</th>
+                <th>Target</th>
+                <th>Product Id</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>SKU001</td>
-                <td>10</td>
-                <td>2021-01-01</td>
-                <td>
-                    <select>
-                        <option value="1">In Progress</option>
-                        <option value="2">Completed</option>
-                        <option value="3">Open</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>SKU002</td>
-                <td>20</td>
-                <td>2021-01-02</td>
-                <td>
-                    <select>
-                        <option value="3">Open</option>
-                        <option value="1">In Progress</option>
-                        <option value="2">Completed</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>SKU003</td>
-                <td>30</td>
-                <td>2021-01-03</td>
-                <td>
-                    <select>
-                        <option value="2">Completed</option>
-                        <option value="1">In Progress</option>
-                        <option value="3">Open</option>
-                    </select>
-                </td>
-            </tr>
+            <?php
+            if ($production_plan->num_rows > 0) {
+                while ($row = $production_plan->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['amount'] . "</td>";
+                    echo "<td>" . $row['created_at'] . "</td>";
+                    echo "<td>" . $row['status'] . "</td>";
+                    echo "<td>" . $row['priority'] . "</td>";
+                    echo "<td>" . $row['target'] . "</td>";
+                    echo "<td>" . $row["product_id"] . "</td>";
+                    echo "</tr>";
+                }
+            }
+            ?>
         </tbody>
 </body>
+
+<script>
+    $('#production_form').submit(function () {
+        var post_data = $('#production_form').serialize();
+        console.log(post_data);
+        $.post('/server/process_production.php', post_data, function (data) {
+            // $('#notification').show();
+            console.log(data);
+            location.reload(); // Reload the page after getting the response
+        });
+    });
+</script>
+
 
 </html>
---
+++ server/process_production.php
@@ -0,0 +1,41 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/database.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+    $requestBody = file_get_contents('php://input');
+    $data = json_decode($requestBody, true);
+
+    // echo json_encode($data);
+    $production_item_id = $_POST["id"];
+
+    $sql = "UPDATE production_plan SET status = 'COMPLETED' WHERE id = $production_item_id";
+
+    $conn->query($sql);
+
+    $production_item = $conn->query("SELECT * from production_plan where id = $production_item_id");
+
+    if ($production_item->num_rows == 1) {
+        $row = $production_item->fetch_assoc();
+        $target = $row['target'];
+        $product_id = $row['product_id'];
+        $amount = $row['amount'];
+        if ($target == "STORAGE") {
+            $sql = "START TRANSACTION; 
+                INSERT INTO storage_logs 
+                (product_id, storage_id, order_id, amount, detail) 
+                VALUES ($product_id, 1, NULL, $amount, 'PRODUCTION_IN');
+                UPDATE products SET storage_amount = storage_amount + $amount WHERE id = $product_id;
+                COMMIT;";
+
+            $conn->multi_query($sql);
+            echo $sql;
+        }
+    } else {
+        echo "Production item not found";
+    }
+
+
+}
