53140822d150c51927e0da45ebc0b4b1c36306f1
laurinbrechter <brechterlaurin@gmail.com>
2024-05-12 14:09:16 +0200

    choose customer in checkout page

--- sp/checkout/index.php
+++ sp/checkout/index.php
@@ -27,6 +27,18 @@
         </tbody>
     </table>
     <button id="buy-btn">Buy</button>
+    <?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/database.php';
+
+    $customers = $conn->query("SELECT * FROM customers;");
+    // select button for customer
+    echo "<select id='customer-id'>";
+    while ($row = $customers->fetch_assoc()) {
+        echo "<option value='" . $row['id'] . "'>" . $row['name'] . "</option>";
+    }
+    echo "</select>";
+
+    ?>
 
 </body>
 
@@ -66,7 +78,15 @@
         var existingItems = JSON.parse(sessionStorage.getItem('items')) || [];
         console.log("buying items", existingItems)
 
-        $.post('/server/process_order.php', { 'items': JSON.stringify(existingItems) }, function (data) {
+        req_body = {
+            'items': existingItems,
+            'sp_id': 1,
+            'customer_id': 1 //$('#customer-id').val()
+        }
+
+        console.log(req_body)
+
+        $.post('/server/process_order.php', req_body, function (data) {
             // $('#notification').show();
             console.log(data)
 
