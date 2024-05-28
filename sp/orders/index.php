60b1a51d1a60139bb27a871a163f698579fab14c
laurinbrechter <brechterlaurin@gmail.com>
2024-05-28 10:33:37 +0200

    filter for sp_id in orders

--- sp/orders/index.php
+++ sp/orders/index.php
@@ -6,9 +6,24 @@ include $_SERVER['DOCUMENT_ROOT'] . '/server/document_head.php';
 
 
 <body>
-    <?php include $_SERVER['DOCUMENT_ROOT'] . '/sp/sp_navbar.php'; ?>
+    <?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/server/database.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/server/decode_jwt.php';
 
-    <?php include $_SERVER['DOCUMENT_ROOT'] . '/server/database.php';
+    $payload = getJwtPayload($_COOKIE["jwt"], 'SERVICE_PARTNER');
+
+    $user_id = $payload->user_id;
+    $sp = $conn->query("SELECT * FROM service_partners WHERE user_id = $user_id;")->fetch_assoc();
+    $sp_id = $sp['id'];
+    $username = $sp["name"];
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/sp/sp_navbar.php';
+
+    if ($sp_id == null) {
+        echo "No service partner found for user_id $user_id";
+        exit();
+    }
+
 
     // TODO filter for sp_id in orders table
     $order_items = $conn->query("SELECT 
@@ -19,49 +34,52 @@ include $_SERVER['DOCUMENT_ROOT'] . '/server/document_head.php';
     oi.product_id 
     FROM 
     orders o
-    left join order_items oi on o.id = oi.order_id;");
+    left join order_items oi on o.id = oi.order_id
+    where o.sp_id = $sp_id;");
 
     ?>
 
     <h1>Orders</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Order Id</th>
-                <th>Order Date</th>
-                <th>Est. Delivery</th>
-                <th>Status</th>
-                <th>Amount</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            if ($order_items->num_rows > 0) {
-                $current_order_id = null;
-                while ($row = $order_items->fetch_assoc()) {
-                    if ($row['order_id'] !== $current_order_id) {
-                        if ($current_order_id !== null) {
-                            echo "<tr><td colspan='5'></td></tr>";
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Order Id</th>
+                    <th>Order Date</th>
+                    <th>Est. Delivery</th>
+                    <th>Status</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($order_items->num_rows > 0) {
+                    $current_order_id = null;
+                    while ($row = $order_items->fetch_assoc()) {
+                        if ($row['order_id'] !== $current_order_id) {
+                            if ($current_order_id !== null) {
+                                echo "<tr><td colspan='5'></td></tr>";
+                            }
                         }
+                        echo "<tr>";
+                        echo "<td>" . $row['order_id'] . "</td>";
+                        echo "<td>" . $row['created_at'] . "</td>";
+                        echo "<td>" . $row['est_delivery'] . "</td>";
+                        echo "<td>" . $row['status'] . "</td>";
+                        echo "<td>" . $row['amount'] . "</td>";
+                        echo "</tr>";
+                        $current_order_id = $row['order_id'];
+                        // echo "<tr>";
+                        // echo "<td colspan='5'>" . $row['item_name'] . "</td>";
+                        // echo "</tr>";
                     }
-                    echo "<tr>";
-                    echo "<td>" . $row['order_id'] . "</td>";
-                    echo "<td>" . $row['created_at'] . "</td>";
-                    echo "<td>" . $row['est_delivery'] . "</td>";
-                    echo "<td>" . $row['status'] . "</td>";
-                    echo "<td>" . $row['amount'] . "</td>";
-                    echo "</tr>";
-                    $current_order_id = $row['order_id'];
-                    // echo "<tr>";
-                    // echo "<td colspan='5'>" . $row['item_name'] . "</td>";
-                    // echo "</tr>";
+                } else {
+                    echo "<tr><td colspan='5'>No orders found</td></tr>";
                 }
-            } else {
-                echo "<tr><td colspan='5'>No orders found</td></tr>";
-            }
-            ?>
-        </tbody>
-    </table>
+                ?>
+            </tbody>
+        </table>
+    </div>
 
 </body>
 
