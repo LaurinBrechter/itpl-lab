996de9309e958f79892eb941604e425b66076587
g0glus <jan.khalo@gmx.de>
2024-05-30 17:17:26 +0200

    Sinnvolle Produkte und Kategorien

--- init_db.sql
+++ init_db.sql
@@ -19,6 +19,7 @@ CREATE TABLE users (
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
+	category VARCHAR(255),
     price DECIMAL(10, 2),
     production_duration INT,
     description TEXT,
