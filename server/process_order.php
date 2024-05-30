cbb4c3fd5ff78f2cb51d9b51ef2926c660ea35c6
laurinbrechter <brechterlaurin@gmail.com>
2024-05-30 13:32:02 +0200

    fix(order_processing): correct storage_facility assignment

--- server/process_order.php
+++ server/process_order.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!$customer) {
         http_response_code(404);
         echo '{"success": false, "msg": "Customer not found"}';
-        exit();
+        exit(1);
     }
 
     if ($customer["isVip"] == 1) {
@@ -50,7 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $sql = "
     START TRANSACTION;
-    INSERT INO orders (sp_id, status, customer_id, priority) 
+    INSERT INTO orders (sp_id, status, customer_id, priority) 
     VALUES ($sp_id, 'PENDING', $customer_id, '$priority');
     SET @order_id = LAST_INSERT_ID();
     $sql_order_items;
@@ -61,29 +61,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $res = $conn->multi_query($sql);
 
-
-
-
-    // Fetch all results of the multi-query
-    do {
-        if ($result = $conn->store_result()) {
-            // if (!$result) {
-            //     http_response_code(500);
-            //     echo "Couldn't create order";
-            // }
-            $result->free();
-        }
-    } while
-    (
-        // $conn->more_results() &&
-        $conn->next_result()
-    );
-
-    exit(0);
+    while ($conn->next_result()) {
+        ;
+    } // flush multi_queries
 
 
     if ($conn->affected_rows === -1) {
         echo "Error: " . $conn->error;
+        exit(1);
     } else {
         $order_id_query = "SELECT @order_id as order_id";
         $order_id_result = $conn->query($order_id_query);
@@ -103,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         left join test_db.products p on oi.product_id = p.id
     )
     select 
-        if(ps.new_amount >= 0, ps.amount, ps.storage_amount) as to_send, -- Adjust this line as needed
+        if(ps.new_amount >= 0, ps.amount, ps.storage_amount) as to_send,
         case 
             when ps.new_amount - 10 >= 0 then 0
             when ps.new_amount <= 10 and ps.new_amount >= 0 then 10 - ps.new_amount + 5
@@ -116,6 +101,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $production_plan = $conn->query($production_plan_sql);
 
     // check if query went through
+    if (!$production_plan) {
+        echo "Error: " . $conn->error;
+        exit(1);
+    }
 
 
 
@@ -125,10 +114,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $isStorageOrder = false;
     $isProductionOrder = false;
+
+    // loop through the production plan, each iteration is one order_item/product
     foreach ($production_plan as $row) {
-        $to_send = $row['to_send'];
-        $to_produce_store = $row['to_produce_store'];
-        $to_produce_cust = $row['to_produce_cust'];
+        $to_send = $row['to_send']; // what to send to the customer
+        $to_produce_store = $row['to_produce_store']; // what to produce for the storage
+        $to_produce_cust = $row['to_produce_cust']; // what to produce for the customer -> higher prio
         $product_id = $row['product_id'];
         $order_id = $row['order_id'];
 
@@ -137,10 +128,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // TODO needs to be added back if e.g. the order is cancelled
             $conn->query("UPDATE products SET storage_amount = storage_amount - $to_send WHERE id = $product_id");
 
-            // reserve the amount in the storage
-            $conn->query("INSERT INTO storage_logs (product_id, storage_id, order_id, amount, detail) 
-            VALUES ($product_id, 1, $order_id, -$to_send, 'RESERVED')
+
+            // figure out from which storage facility we can take the product from
+            // note that at this point we already know that facility storage will be enough to cover the demand
+            $facility_storage = $conn->query("select storage_id, sum(amount) as total_amount, product_id  
+            from storage_logs sl
+            where product_id = $product_id
+            group by storage_id, product_id
+            having sum(amount) > 0
             ");
+
+            // reserve the amount in the storage facilities until the amount we need is reached.
+            foreach ($facility_storage as $row) {
+                $storage_id_rand = $row['storage_id'];
+                $amount = $row['total_amount'];
+                $conn->query("INSERT INTO storage_logs (product_id, storage_id, order_id, amount, detail) 
+                VALUES ($product_id, $storage_id_rand, $order_id, -$to_send, 'RESERVED')
+                ");
+                $to_send -= $amount;
+                if ($to_send <= 0) {
+                    break;
+                }
+            }
+
         }
         if ($to_produce_store > 0) {
             $conn->query("INSERT INTO 
