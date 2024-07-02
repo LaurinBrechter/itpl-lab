fcbae6f933a6e6b69ff7069d9c222c58dd3eb666
g0glus <jan.khalo@gmx.de>
2024-07-02 16:28:55 +0200

    style

--- management/add_servicepartner.php
+++ management/add_servicepartner.php
@@ -3,14 +3,22 @@ include $_SERVER['DOCUMENT_ROOT'] . '/server/database.php';
 
 // Daten aus dem Formular empfangen
 $name = $_POST['name'];
+$tax_number = $_POST['tax_number'];
+$isInternal = $_POST['isInternal'];
 $street = $_POST['street'];
 $house_number = $_POST['house_number'];
 $city = $_POST['city'];
 $state = $_POST['state'];
 $zip = $_POST['zip'];
 $country = $_POST['country'];
-$telephone_number = $_POST['telephone_number'];
-$isVip = $_POST['isVip'];
+
+// User in Datenbank anlegen
+$sql = "INSERT INTO users (username, role) VALUES (?, ?)";
+$stmt = $conn->prepare($sql);
+$role = 'SERVICE_PARTNER';
+$stmt->bind_param("ss", $name, $role);
+$stmt->execute();
+$user_id = $stmt->insert_id; // Die ID des gerade eingefügten Benutzers erhalten
 
 // Adresse in die Datenbank einfügen
 $sql = "INSERT INTO addresses (street, house_number, city, state, zip, country) VALUES (?, ?, ?, ?, ?, ?)";
@@ -19,16 +27,16 @@ $stmt->bind_param("ssssss", $street, $house_number, $city, $state, $zip, $countr
 $stmt->execute();
 $address_id = $stmt->insert_id; // Die ID der gerade eingefügten Adresse erhalten
 
-// Kunden in die Datenbank einfügen
-$sql = "INSERT INTO customers (name, address_id, telephone_number, isVip) VALUES (?, ?, ?, ?)";
+// Servicepartner in die Datenbank einfügen
+$sql = "INSERT INTO service_partners (name, tax_number, address_id, isInternal, user_id) VALUES (?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sisi", $name, $address_id, $telephone_number, $isVip);
+$stmt->bind_param("ssiii", $name, $tax_number, $address_id, $isInternal, $user_id);
 $stmt->execute();
 
 if ($stmt->affected_rows > 0) {
-    echo "Neuer Kunde erfolgreich hinzugefügt.";
+    echo "Neuer Servicepartner erfolgreich hinzugefügt.";
 } else {
-    echo "Fehler beim Hinzufügen des Kunden: " . $conn->error;
+    echo "Fehler beim Hinzufügen des Servicepartners: " . $conn->error;
 }
 
 $stmt->close();
--- management/index.php
+++ management/index.php
@@ -13,57 +13,63 @@ include $_SERVER['DOCUMENT_ROOT'] . '/server/document_head.php';
     include $_SERVER['DOCUMENT_ROOT'] . '/server/decode_jwt.php';
     $payload = getJwtPayload($_COOKIE["jwt"], ['MANAGEMENT']);
     ?>
-    <h1>Neuen Servicepartner hinzufügen</h1>
-    <form action="/management/add_servicepartner.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br><br>
-        <label for="tax_number">Steuernummer:</label>
-        <input type="text" id="tax_number" name="tax_number"><br><br>
-        <label for="street">Straße:</label>
-        <input type="text" id="street" name="street" required><br><br>
-        <label for="house_number">Hausnummer:</label>
-        <input type="text" id="house_number" name="house_number"><br><br>
-        <label for="city">Stadt:</label>
-        <input type="text" id="city" name="city" required><br><br>
-        <label for="state">Bundesland:</label>
-        <input type="text" id="state" name="state"><br><br>
-        <label for="zip">PLZ:</label>
-        <input type="number" id="zip" name="zip" required><br><br>
-        <label for="country">Land:</label>
-        <input type="text" id="country" name="country" required><br><br>
-        <label for="isInternal">Zugehörigkeit:</label>
-        <select id="isInternal" name="isInternal" required>
-            <option value="1">Intern</option>
-            <option value="0" selected>Extern</option>
-        </select><br><br>
-        <input type="submit" value="Servicepartner hinzufügen">
-    </form>
+    <div class="forms-container">
+        <div class="form-container">
+            <h2>Neuen Servicepartner hinzufügen</h2>
+            <form action="/management/add_servicepartner.php" method="post">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required><br><br>
+                <label for="tax_number">Steuernummer:</label>
+                <input type="text" id="tax_number" name="tax_number"><br><br>
+                <label for="street">Straße:</label>
+                <input type="text" id="street" name="street" required><br><br>
+                <label for="house_number">Hausnummer:</label>
+                <input type="text" id="house_number" name="house_number"><br><br>
+                <label for="city">Stadt:</label>
+                <input type="text" id="city" name="city" required><br><br>
+                <label for="state">Bundesland:</label>
+                <input type="text" id="state" name="state"><br><br>
+                <label for="zip">PLZ:</label>
+                <input type="number" id="zip" name="zip" required><br><br>
+                <label for="country">Land:</label>
+                <input type="text" id="country" name="country" required><br><br>
+                <label for="isInternal">Zugehörigkeit:</label>
+                <select id="isInternal" name="isInternal" required>
+                    <option value="1">Intern</option>
+                    <option value="0" selected>Extern</option>
+                </select><br><br>
+                <input type="submit" value="Servicepartner hinzufügen">
+            </form>
+        </div>
 
-    <h1>Neuen Kunden hinzufügen</h1>
-    <form action="/management/add_customer.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br><br>
-        <label for="street">Straße:</label>
-        <input type="text" id="street" name="street" required><br><br>
-        <label for="house_number">Hausnummer:</label>
-        <input type="text" id="house_number" name="house_number"><br><br>
-        <label for="city">Stadt:</label>
-        <input type="text" id="city" name="city" required><br><br>
-        <label for="state">Bundesland:</label>
-        <input type="text" id="state" name="state"><br><br>
-        <label for="zip">PLZ:</label>
-        <input type="text" id="zip" name="zip" required><br><br>
-        <label for="country">Land:</label>
-        <input type="text" id="country" name="country" required><br><br>
-        <label for="telephone_number">Telefonnummer:</label>
-        <input type="text" id="telephone_number" name="telephone_number"><br><br>
-        <label for="isVip">VIP Status:</label>
-        <select id="isVip" name="isVip" required>
-            <option value="1">Ja</option>
-            <option value="0" selected>Nein</option>
-        </select><br><br>
-        <input type="submit" value="Kunden hinzufügen">
-    </form>
+        <div class="form-container">
+            <h2>Neuen Kunden hinzufügen</h2>
+            <form action="/management/add_customer.php" method="post">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required><br><br>
+                <label for="street">Straße:</label>
+                <input type="text" id="street" name="street" required><br><br>
+                <label for="house_number">Hausnummer:</label>
+                <input type="text" id="house_number" name="house_number"><br><br>
+                <label for="city">Stadt:</label>
+                <input type="text" id="city" name="city" required><br><br>
+                <label for="state">Bundesland:</label>
+                <input type="text" id="state" name="state"><br><br>
+                <label for="zip">PLZ:</label>
+                <input type="text" id="zip" name="zip" required><br><br>
+                <label for="country">Land:</label>
+                <input type="text" id="country" name="country" required><br><br>
+                <label for="telephone_number">Telefonnummer:</label>
+                <input type="text" id="telephone_number" name="telephone_number"><br><br>
+                <label for="isVip">VIP Status:</label>
+                <select id="isVip" name="isVip" required>
+                    <option value="1">Ja</option>
+                    <option value="0" selected>Nein</option>
+                </select><br><br>
+                <input type="submit" value="Kunden hinzufügen">
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
