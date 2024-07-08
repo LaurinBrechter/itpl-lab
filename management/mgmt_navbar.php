4a79240b4895ff24c06ad505b63ff61a2f8dd379
g0glus <jan.khalo@gmx.de>
2024-07-08 22:40:17 +0200

    mgmt navbar auf englisch

--- management/mgmt_navbar.php
+++ management/mgmt_navbar.php
@@ -3,9 +3,9 @@
         <div class="logo"></div>
         <h1>Management</h1>
         <div class="navitems">
-            <div><a href="/management/overview/index.php">Übersicht</a></div>
-            <div><a href="/management">Hinzufügen</a></div>
-            <div><a href="/sp/catalog/index.php">Katalog</a></div>
+            <div><a href="/management/overview/index.php">Overview</a></div>
+            <div><a href="/management">Add Account</a></div>
+            <div><a href="/sp/catalog/index.php">Catalog</a></div>
             <div><a href="/management/dashboard/index.php">Dashboard</a></div>
         </div>
     </ul>
